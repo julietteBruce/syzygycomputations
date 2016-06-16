053b3103c15381673ceb1e3470d0b5193c3622d6
Jay Yang <jkelleyy@gmail.com>
2016-06-16 12:53:10 -0500

    add up to date final_qr.m

--- src/condor/final_qr.m
+++ src/condor/final_qr.m
@@ -1,22 +1,40 @@
 
+function final_qr(inFile,outFile)
 
 
+function y = qr_rank(inFile)
 %%% Loads Matrix From Given File %%%
-sparse_data load(inFile)
+sparse_data = load(inFile);
 T = spconvert(sparse_data);
 
 %%% Computes QR Factorization %%%
 [m,n] = size(T);
 q=colamd(T, [n m]);
-[Q,R,P] = qr(T(:,q));
+T=T(:,q);
+T(:,~any(T,1)) = [];
+[m,n] = size(T);
+[Q,R,P] = qr(T);
+
 
 %%% Computes Rank from QR Factorization %%%
 tol = max(size(T))*eps*abs(R(1,1));
 r = 0;
-while ( abs(R(r+1,r+1)) >= tol & r < min(n,m)+1 )
-r = r+1;
+while (r < min(n,m) & abs(R(r+1,r+1)) >= tol)
+    r = r+1;
 end
 
 %%% Saves Rank to MatLab File %%%
-save('test_2_5_7_2_multidegree_20_18_7.mat','r')
-r
+%save('test_2_5_7_2_multidegree_20_18_7.mat','r')
+y=r;
+end
+
+outFd = fopen(outFile,'w');
+
+res = qr_rank(inFile);
+fprintf(outFd,'%d',res);
+
+fclose(outFd);
+
+
+
+end
