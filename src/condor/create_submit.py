908384a63711b5d92d570712c9362be6123ac7a2
Jay Yang <jkelleyy@gmail.com>
2016-08-01 11:59:24 -0500

    fix issue with braces in a format string

--- src/condor/create_submit.py
+++ src/condor/create_submit.py
@@ -11,7 +11,7 @@ log = single_entry_{1}_{2}.$(CLUSTER).log
 
 ### from slide 21 of
 ##    https://twiki.opensciencegrid.org/twiki/pub/Education/UserSchool16Materials/gthain-2016-HTCondor-3.ppt
-request_memory = ifthenelse(MemoryUsage =!= undefined,max({2048, (MemoryUsage * 3/2)}), 2048)
+request_memory = ifthenelse(MemoryUsage =!= undefined,max({{2048, (MemoryUsage * 3/2)}}), 2048)
 periodic_hold = (MemoryUsage >= ((RequestMemory) * 5/4 )) && (JobStatus == 2)
 periodic_release = (JobStatus == 5) && ((CurrentTime - EnteredCurrentStatus) > 180) && (NumJobStarts < 5) && (HoldReasonCode =!= 13) && (HoldReasonCode =!= 34)
 
