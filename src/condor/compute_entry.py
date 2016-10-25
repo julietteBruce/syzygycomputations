8758c207a738452e7691d0c93963f3aa92ec1a1d
Jay Yang <jkelleyy@gmail.com>
2016-10-25 13:45:13 -0500

    update compute_entry.py

--- src/condor/compute_entry.py
+++ src/condor/compute_entry.py
@@ -4,6 +4,7 @@ import os
 import os.path
 import glob
 import re
+import sys
 
 def binom(n,k):
     val = 1
@@ -35,29 +36,45 @@ def get_md_rank(p,q,md):
             rnk = int(f.read())
             return rnk;
     except IOError:
-        return 0
+        #if the matrix exists, return Nan otherwise 0
+        if os.path.isfile("./matricies/map_{0}_{1}/multidegree_{2}_{3}_{4}.dat".format(p,q,*sorted(md))):
+            return float('NaN');
+        else:
+            return 0;
+    except ValueError:
+        return float('NaN');
 
 def get_rank(p,q):
     tot = 0
-    
+    isValid = True
     for fname in glob.glob("./out_{0}_{1}/multidegree_*.txt".format(p,q)):
         #FIXME this only works for n=3 right now
         match = re.search('multidegree_(\d+)_(\d+)_(\d+).txt',fname)
         md = (int(match.group(1)),int(match.group(2)),int(match.group(3)))
         with open(fname,'r') as f:
-            rnk = int(f.read())
+            try:
+                rnk = int(f.read())
+                #print(md)
+                #print(rnk)
+                tot += rnk*multiplicity(md)
 
-            print(md)
-            print(rnk)
-            tot += rnk*multiplicity(md)
-    print(tot)
-    return tot
+            except ValueError:
+                isValid = False
+                #print(md)
+                #print(None)
+    if isValid:
+        #print(tot)
+        return tot
+    else:
+        #print(None)
+        return float('nan');
 
 
 argparser = argparse.ArgumentParser();
 
 argparser.add_argument('n')
 argparser.add_argument('d')
+argparser.add_argument('k')
 argparser.add_argument('p')
 argparser.add_argument('q')
 argparser.add_argument('--md')
@@ -65,13 +82,14 @@ args = argparser.parse_args()
 
 n=int(args.n)
 d=int(args.d)
+k=int(args.k)
 p=int(args.p)
 q=int(args.q)
 
 if(args.md):
     string_md = tuple(args.md.split(','))
     #TODO temporary hack to get some numbers, we need to store the size of matricies
-    fname_glob = "../../test_5/matricies/map_{0}_{1}/multidegree_{2}_{3}_{4}.dat".format(p,q,*sorted(string_md))
+    fname_glob = "./matricies/map_{0}_{1}/multidegree_{2}_{3}_{4}.dat".format(p,q,*sorted(string_md))
     for fname in glob.glob(fname_glob):
         #FIXME this only works for n=3 right now
         match = re.search('multidegree_(\d+)_(\d+)_(\d+).dat',fname)
@@ -86,9 +104,9 @@ if(args.md):
         ker_rank = md_basis_size - get_md_rank(p,q,md)
         img_rank = get_md_rank(p+1,q-1,md)
         print("{} {}".format(md,ker_rank-img_rank))
-        
+        sys.stdout.flush()
 else:
-    basis_size = binom(binom(d+n,n),p)*binom(d*q+n,n);
+    basis_size = binom(binom(d+n,n),p)*binom(d*q+k+n,n);
     ker_rank = basis_size - get_rank(p,q)
     img_rank = get_rank(p+1,q-1)
     print(ker_rank-img_rank)
