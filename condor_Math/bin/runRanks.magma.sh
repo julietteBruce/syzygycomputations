b7b36a9fc795c0136bf47e64aa0baff2d340b79f
Steve Goldstein <sgoldstein@math.wisc.edu>
2019-06-03 11:14:14 -0500

    Added support for GF(p) in magma rank calculation; added another potentially problematic example rank calculation

--- condor_Math/bin/runRanks.magma.sh
+++ condor_Math/bin/runRanks.magma.sh
@@ -2,13 +2,21 @@
 
 infile=$1
 rankScript=$2
+### if p = 0  RationalField() for the rank calculation;  otherwise GF(p) 
+if [ $# -eq 3 ]
+then
+    p=$3
+else
+    ## maybe the default should be p = 32003
+    p=0
+fi
 
 ## unzip $infile into /tmp
 infile_unzipped=/tmp/$(basename -s .gz $infile)
 /bin/zcat -f $infile > $infile_unzipped
 
 ## compute rank
-/usr/local/bin/magma -b file:=$infile_unzipped $rankScript
+/usr/local/bin/magma -b file:=$infile_unzipped p:=$p $rankScript
 ## this probably doesn't work because MAGMA might not follow exit status conventions...
 magmaStatus=$?
 
