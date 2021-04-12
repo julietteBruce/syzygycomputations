16d3de1c0b8e418046c502b058b58b630f366313
Daniel Corey <dcorey2814@gmail.com>
2021-04-12 15:00:36 -0500

    updated dataRange.m2 and getData.py

--- P1P1Syzygies/getData.py
+++ P1P1Syzygies/getData.py
@@ -4,7 +4,6 @@ import itertools
 from os import listdir
 from os.path import isfile, join
 import os
-import argparse
 import glob
 
 
@@ -16,11 +15,18 @@ def filename_to_indices(name):
 def dFirst(bd):
     return tuple([bd[2],bd[3],bd[0],bd[1]])
 
-onlyfiles = glob.glob(os.path.join("HirzebruchSyzygies","*"))
+onlyfiles = glob.glob(os.path.join("P1P1Syzygies","bettiF0*"))
 bds = list(set([filename_to_indices(f) for f in onlyfiles]))
 bds.sort(key=dFirst)
+
+def count(D):
+    return len([BD for BD in bds if [BD[2],BD[3]]==D])
 
 with open("dataRange.m2","w") as dataRangeFile:
     dataRangeFile.write("{\n")
     dataRangeFile.write(",\n".join(["{{"+"{},{}".format(bd[0],bd[1])+"},{"+"{},{}".format(bd[2],bd[3])+"}}" for bd in bds]))
     dataRangeFile.write("\n}")
+    
+    
+    
+
